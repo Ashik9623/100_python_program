73132b4e44b1b8852bf3a07220cf9487cbd9adfa
ashik-islam <ashik8090100@gmail.com>
2024-01-14 01:01:19 +0600

    Celsius to fahrenheit

---
+++ 09_celsius_to_fahrenheit.py
@@ -0,0 +1,9 @@
+#Python Program to convert temperature in celsius to fahrenheit
+
+#change this valu for a different result
+
+celsius = 37.5
+
+#calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print("%0.1f degree celsius to %0.1f degree Fahrenheit" %(celsius, fahrenheit))
