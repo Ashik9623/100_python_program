0957e6416f8d3d5f669394f989915b7e1587fb2d
ashik-islam <ashik8090100@gmail.com>
2024-01-18 22:04:25 +0600

    Natural number

---
+++ 14_check_prime_number.py
@@ -0,0 +1,18 @@
+num = int(input("Enter the number:"))
+
+flag = False
+
+if num == 1:
+    print(num, "is not a prime number: ")
+elif num > 1:
+    #check for factors
+    for i in range(2, num):
+        if(num % i == 0):
+            # if factor is found, set flag to true
+            flag = True
+            #break out of loop
+            break
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
--- 17_multiplication.py
+++ 17_multiplication.py
@@ -1,3 +1,4 @@
 num = int(input("Enter your number: "))
 for i in range(1,11):
     print(num, 'X', i, '=', num * i)
+    
